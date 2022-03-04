c924c868f0417f152507917e490aa0f266520dea
= <=>
2022-03-04 21:51:47 +0530

    Types have been added

--- frontend/components/HeroSection/FeatureCard.tsx
+++ frontend/components/HeroSection/FeatureCard.tsx
@@ -1,4 +1,9 @@
-export const FeatureCard = ({ title, text }) => {
+interface Props {
+    title: string,
+    text: string
+}
+
+export const FeatureCard = (props: Props) => {
     return (
         <>
             <div className="flex py-5 my-4  lg:m-0 max-w-[338px] m-auto">
@@ -6,8 +11,8 @@ export const FeatureCard = ({ title, text }) => {
                     logo
                 </div>
                 <div className="ml-[5px]  ">
-                    <p className="font-bold">{title}</p>
-                    <p className="text-left">{text}</p>
+                    <p className="font-bold">{props.title}</p>
+                    <p className="text-left">{props.text}</p>
                 </div>
             </div>
         </>
--- frontend/components/HeroSection/HeroSection.tsx
+++ frontend/components/HeroSection/HeroSection.tsx
@@ -4,7 +4,7 @@ import { HeroText } from "./HeroText"
 export const HeroSection = () => {
     return (
         <>
-            <div className=" py-[150px] w-[90%] m-auto lg:w-[90%]">
+            <div className=" lg:py-[100px] w-[90%] m-auto lg:w-[90%]">
                 <HeroText />
                 <Features />
             </div>
